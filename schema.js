39d01d5764a3721346680fc116b74fa9ce3ebce6
mnindra <mnindrazaka@gmail.com>
2017-12-26 15:06:54 +0700

    mutation data

--- schema.js
+++ schema.js
@@ -43,6 +43,51 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addCustomer: {
+      type: CustomerType,
+      args: {
+        name: {type: new GraphQLNonNull(GraphQLString)},
+        email: {type: new GraphQLNonNull(GraphQLString)},
+        age: {type: new GraphQLNonNull(GraphQLInt)}
+      },
+      resolve(parentValue, args) {
+        return axios.post('http://localhost:3000/customers', {
+          name: args.name,
+          email: args.email,
+          age: args.age,
+        }).then(response => response.data);
+      }
+    },
+    deleteCustomer: {
+      type: CustomerType,
+      args: {
+        id: {type: new GraphQLNonNull(GraphQLString)}
+      },
+      resolve(parentValue, args) {
+        return axios.delete('http://localhost:3000/customers/' + args.id)
+          .then(response => response.data);
+      }
+    },
+    editCustomer: {
+      type: CustomerType,
+      args: {
+        id: {type: new GraphQLNonNull(GraphQLString)},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        age: {type: GraphQLInt}
+      },
+      resolve(parentValue, args) {
+        return axios.patch('http://localhost:3000/customers/' + args.id, args)
+          .then(response => response.data);
+      }
+    }
+  }
+});
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 });
